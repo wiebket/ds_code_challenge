1c7ae760607aec21c026e0ba593659f7e646a026
wiebket <wiebke.toussaint@gmail.com>
2025-10-19 23:19:31 +0200

    feature: add logging for dataframe join

--- ds_code_challenge/data/transform.py
+++ ds_code_challenge/data/transform.py
@@ -13,14 +13,30 @@ def spatial_join(sr_df, hex_df):
     logger = logging.getLogger(__name__)
 
     # Convert pandas df to GeoDataFrame
-    sr_df['geometry'] = sr_df.apply(lambda row: Point(row['longitude'], row['latitude']), axis=1)
-    sr_geo_df = gpd.GeoDataFrame(sr_df, geometry='geometry', crs='EPSG:4326')
+    sr_df["geometry"] = sr_df.apply(lambda row: Point(row["longitude"], row["latitude"]), axis=1)
+    sr_geo_df = gpd.GeoDataFrame(sr_df, geometry="geometry", crs="EPSG:4326")
 
     # Rename index column in hex8
-    hex_df.rename(columns={'index':'h3_level8_index'}, inplace=True)
+    hex_df.rename(columns={"index": "h3_level8_index"}, inplace=True)
 
     # Spatial join to find intersection (i.e. any overlap) between service request geometry & hexagon geometry
-    sr_hex_df = gpd.sjoin(sr_geo_df, hex_df, how='left', predicate='intersects', rsuffix='right')
+    sr_hex_df = gpd.sjoin(sr_geo_df, hex_df, how="left", predicate="intersects", rsuffix="right")
+
+    # Check for failed joins (NaN in joined columns)
+    failed_joins = sr_hex_df[sr_hex_df["index_right"].isna()]
+    join_error = len(failed_joins) / len(sr_geo_df) * 100
+
+    # Log results
+    if len(failed_joins) > 0:
+        logger.warning(
+            f"Failed to join {len(failed_joins)} out of {len(sr_geo_df)} rows ({join_error:.2f}%)."
+        )
+    elif join_error > 22.56:
+        raise ValueError(
+            "Aborting. Join failure rate exceeds missing latitude/longitude values. Check join syntax."
+        )
+    else:
+        logger.info(f"Successfully joined all {len(sr_geo_df)} rows")
 
     return sr_hex_df
 
